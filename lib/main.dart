bd73a34d89b7f90d637632c1a3f044532698a5d5
Ola2024Mohammed <hawaiiict2022@gmail.com>
2024-09-22 02:08:28 +0300

    Clean Architecture Structure

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TennisPlayerApp());
+}
+
+class TennisPlayerApp extends StatelessWidget {
+  const TennisPlayerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
